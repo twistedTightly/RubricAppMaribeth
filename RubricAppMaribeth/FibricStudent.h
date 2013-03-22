ac16c961bdbb36c4314dadd993caebf09a1fd303
Maribeth <mrauh1@nd.edu>
2013-03-22 13:13:02 -0400

    Student object added

---
+++ RubricAppMaribeth/FibricStudent.h
@@ -0,0 +1,17 @@
+//
+//  FibricStudent.h
+//  RubricAppMaribeth
+//
+//  Created by Maribeth Rauh on 3/22/13.
+//  Copyright (c) 2013 Maribeth Rauh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FibricStudent : NSObject
+
+@property (nonatomic, strong) NSString* studentFirstName;
+@property (nonatomic, strong) NSString* studentLastName;
+@property (nonatomic, strong) NSString* yourClass;
+
+@end
---
+++ RubricAppMaribeth/FibricStudent.m
@@ -0,0 +1,13 @@
+//
+//  FibricStudent.m
+//  RubricAppMaribeth
+//
+//  Created by Maribeth Rauh on 3/22/13.
+//  Copyright (c) 2013 Maribeth Rauh. All rights reserved.
+//
+
+#import "FibricStudent.h"
+
+@implementation FibricStudent
+
+@end
