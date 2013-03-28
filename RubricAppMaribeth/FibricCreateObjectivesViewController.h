4b4905d8ae111026361f6a51200cc858c749b787
KanebCenter <mrauh1@nd.edu>
2013-03-28 14:33:10 -0400

    CreateObjectives view and VC added

---
+++ RubricAppMaribeth/FibricCreateObjectivesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FibricCreateObjectivesViewController.h
+//  RubricAppMaribeth
+//
+//  Created by Learning Technology Lab on 3/28/13.
+//  Copyright (c) 2013 Maribeth Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FibricCreateObjectivesViewController : UITableViewController
+
+@end
